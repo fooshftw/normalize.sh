edea6e43e3bbd2adf727f1ba38f756d7154dc084
Rob Abbe <roba@Rabbe-LW-WKS.(none)>
2014-04-09 09:58:48 -0400

    more tidying

--- normalize.sh
+++ normalize.sh
@@ -18,12 +18,9 @@ if [ -z "$Project" ];
         Files=(base forms grid headings links lists media tables typography utility)
 
         S_User=$(env | grep SUDO_USER | cut -d"=" -f2)
-	Base=/home/${S_USER}/${Project}/base
-        mkdir -p "$Base"
+	Base=/home/${S_User}/${Project}/base
+        mkdir -p $Base
 	cd $Base
-        touch "_${Files[@]/%/.scss}"
-        chown -R ${S_User}:${S_User} ~/${Project}
-
          for i in "${Files[@]}"
            do
              echo "/* ==========================================================================" >> ${Base}/_${i}.scss
@@ -38,23 +35,25 @@ if [ -z "$Project" ];
 #Extract specific information to each file:
 
 #base
-  echo "\**" >> $Base/_base.scss
-  tail -n +2 ~/normalize.css | sed -n "/san/,/HTML5/p" | head -n -1 >> $Base/_base.scss
+	echo "\**" >> $Base/_base.scss
+	tail -n +2 ~/normalize.css | sed -n "/san/,/HTML5/p" | head -n -1 >> $Base/_base.scss
 
 #forms
-  tail -n +2 ~/normalize.css | sed -n "/Forms/,/Tables/p" | head -n -1 | tail -n +3 >> $Base/_forms.scss
+	tail -n +2 ~/normalize.css | sed -n "/Forms/,/Tables/p" | head -n -1 | tail -n +3 >> $Base/_forms.scss
 
 #tables
-  tail -n +2 ~/normalize.css | sed -n '/Tables/,$p' | tail -n +3 >> $Base/_tables.scss
+	tail -n +2 ~/normalize.css | sed -n '/Tables/,$p' | tail -n +3 >> $Base/_tables.scss
 
 #links
-  tail -n +2 ~/normalize.css | sed -n "/Links/,/Text-level/p" | head -n -1 | tail -n +3 >> $Base/_links.scss
+	tail -n +2 ~/normalize.css | sed -n "/Links/,/Text-level/p" | head -n -1 | tail -n +3 >> $Base/_links.scss
 
 #media
-  tail -n +2 ~/normalize.css | sed -n "/Embedded/,/Forms/p" | head -n -1 | tail -n +3 >> $Base/_media.scss
+	tail -n +2 ~/normalize.css | sed -n "/Embedded/,/Forms/p" | head -n -1 | tail -n +3 >> $Base/_media.scss
 
 #typography
-  tail -n +2 ~/normalize.css | sed -n "/Grouping/,/Forms/p" | head -n -1 | tail -n +3 >> $Base/_typography.scss
+	tail -n +2 ~/normalize.css | sed -n "/Grouping/,/Forms/p" | head -n -1 | tail -n +3 >> $Base/_typography.scss
+
+	chown -R ${S_User}:${S_User} /home/${S_User}/${Project}
 
 fi
 
